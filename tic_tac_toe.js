1d3db7c5198cdff2a45af4ed2a63f656af527735
omarupk33 <kkomar12334@gmail.com>
2025-07-14 21:53:33 +0300

    message

---
+++ tic_tac_toe.js
@@ -0,0 +1,22 @@
+// function tic_tac_toe (board){
+//     const score = 0
+//     const player_one = []
+//     const player_two = []
+//     return Game = () => {
+
+//     }
+// }
+
+
+//  Forsome reason, this is not working
+    let board = []
+    for(let i; i > 10; i++){
+    // if( i%2 === 0){
+    // board.push('|')
+    // }
+    // else{
+    board.push[i]
+    // }
+    }
+
+ console.log(board)
